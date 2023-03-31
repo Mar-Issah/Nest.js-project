69672de36aa36a1d63f133010d43d91b7caf2fbe
Mar-Issah <masy370@gmail.com>
2023-03-31 18:38:07 -0400

    create a new folder with resource command

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrdersModule } from './orders/orders.module';
+import { TestModule } from './test/test.module';
 
 @Module({
-  imports: [OrdersModule],
+  imports: [OrdersModule, TestModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Delete,
+  Put,
+  Param,
+  Query,
+} from '@nestjs/common';
 
 @Controller('orders')
-export class OrdersController {}
+export class OrdersController {
+  @Get()
+  getOrders(@Query('type') type: string) {
+    return [{ type: `Type: ${type}` }];
+  }
+
+  @Get(':id')
+  getOrder(@Param('id') id: number) {
+    return { success: `I have ID: ${id}` };
+  }
+
+  @Post()
+  createOrder() {
+    return { sucess: 'created!' };
+  }
+
+  @Put(':id')
+  upadteOrder(@Param('id') id: number) {
+    return { sucess: 'updated!' };
+  }
+  @Delete(':id')
+  deleteOrder(@Param('id') id: number) {
+    return { sucess: `deleted ID: ${id} !` };
+  }
+}
